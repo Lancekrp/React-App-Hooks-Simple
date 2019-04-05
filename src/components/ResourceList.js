5950dad256ef7fd799d389a088788406b50283d8
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-05 18:55:10 +0800

    componentDidUpdate vs componentDidMount

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -5,8 +5,15 @@ class ResourceList extends React.Component {
   state = {resources: []};
 
   async componentDidMount() {
-  const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
   this.setState({resources: response.data});
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+      this.setState({resources: response.data});
+    }
   }
 
   render() {
