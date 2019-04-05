33620482755729f5c4644885bb527c5a5850b0a5
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-05 19:55:04 +0800

    reuse of code

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const ResourceList = ({resource}) => {
+const useResources = (resource) => {
+
   const [resources, setResources] = useState([]);
 
   // const fetchResource = async (resource) => {
@@ -22,6 +23,13 @@ const ResourceList = ({resource}) => {
     },
     [resource]
   );
+
+  return resources;
+}
+
+const ResourceList = ({resource}) => {
+
+  const resources = useResources(resource);
 
   return (
     <ul>
