dfcc7743c601d7d1102fe1715c78b3a5ffd90627
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-05 19:46:55 +0800

    rendering a list

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -24,9 +24,9 @@ const ResourceList = ({resource}) => {
   );
 
   return (
-    <div>
-      {resources.length}
-    </div>
+    <ul>
+      {resources.map(record => (<li key={record.id}>{record.title}</li>))}
+    </ul>
   );
 }
 
